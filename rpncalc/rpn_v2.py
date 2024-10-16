a245123967aad8b4997b278d4db51b8f85449788
Florian Bruhin <me@the-compiler.org>
2024-10-16 07:17:00 +0200

    Reduce unneeded v2 changes

--- rpncalc/rpn_v2.py
+++ rpncalc/rpn_v2.py
@@ -2,26 +2,22 @@ from __future__ import annotations
 
 import sys
 
-from rpncalc.utils import calc, Config
+from rpncalc.utils import calc
 
 class RPNCalculator:
     def __init__(self, config):
         self.config = config
         self.stack = []
 
-    def get_inputs(self) -> list[str]:
-        inp = input(self.config.prompt + " ")
-        return inp.split()
-
     def run(self) -> None:
         while True:
-            for inp in self.get_inputs():
-                if inp == "q":
-                    return
-                elif inp == "p":
-                    print(self.stack)
-                else:
-                    self.evaluate(inp)
+            inp = input("> ")
+            if inp == "q":
+                return
+            elif inp == "p":
+                print(self.stack)
+            else:
+                self.evaluate(inp)
 
     def err(self, msg: str) -> None:
         print(msg, file=sys.stderr)
@@ -56,7 +52,5 @@ class RPNCalculator:
 
 
 if __name__ == "__main__":
-    config = Config()
-    config.load_env()
-    rpn = RPNCalculator(config)
+    rpn = RPNCalculator()
     rpn.run()
